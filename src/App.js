76c034ec13f4e4c60ba18d4781dacd653d5e8a9d
Akash <akash.choudhary@adda247.com>
2023-05-06 09:41:37 +0530

    Added solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
+
+// https://meet.google.com/fqn-regx-yso
